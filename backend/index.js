d9fb64e587beb4111035f627d30aaa9be225c40e
Arvin <noelarvin96@gmail.com>
2025-01-16 20:37:12 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -15,11 +15,6 @@ const port = 3000;
 app.use(express.json())
 
 app.use(cors())
-app.use(cors({
-    origin: 'https://mern-todo-list1.onrender.com', // Replace with your frontend URL
-    credentials: true,
-  }));
-  
 
 RunServer()
 
