3e447fcded4c1361714e7ad1669a47755b88416b
Arvin <noelarvin96@gmail.com>
2026-01-03 10:51:17 +0530

    updated index.js issue

--- backend/index.js
+++ backend/index.js
@@ -3,34 +3,18 @@ const express = require('express');
 const RunServer = require('./database/connection');
 const cors = require('cors');
 const todoRouter = require('./routes/todolist_routes');
-const app = express();
 
+const app = express();
 const port = process.env.PORT || 3000;
 
-const allowedOrigins = [
-  'https://react-todo-list-4jyj.onrender.com',
-  'http://localhost:5173',
-  'http://51.20.138.101/todolist/'
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      console.log('CORS Error:', origin); // Log the origin causing issues
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
-
-
+app.use(cors());          // simple & correct
 app.use(express.json());
+
 RunServer();
-app.use('/todolist', todoRouter);
+
+// API ROUTES
+app.use('/api', todoRouter);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-  console.log("Server running on port:", process.env.PORT);
-
+  console.log(`Server running on port ${port}`);
 });
